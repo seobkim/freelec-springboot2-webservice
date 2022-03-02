316b3ce3661a8151ec4c01cc132a53e10f5752cb
seobkim <kks7824@gmail.com>
2022-03-02 19:31:23 +0900

    114P Create / Update api

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.jojoldu.book.springboot.service.posts;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +19,28 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
 
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    // Update 기능인데 쿼리를 날리는 부분이 없음.
+    // 이게 가능한 이유는 JPA의 영속성 컨텍스트 때문!
+    // 영속성 컨텍스트란, 엔티티를 영구 저장하는 환경 엔티티가 영속성 컨텍스트에 포함되어 있냐 아니냐로 갈림
+    // 트랜잭션 안에서 데이터를 가져오면 영속성 컨텍스트가 유지된 상태이며 값을 변경하면 트랜잭션이 끝나는 시점에
+    // 해당 테이블에 변경분을 반영함 Entity 객체의 값만 변경하면 별도로 Update 쿼리를 날릴 필요가 없음. 이 개념을 '더티체킹' 이라함
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+
     }
 }
