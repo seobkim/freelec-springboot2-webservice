5f1715cc4eb0ef11eece16b516c31f97d92611ae
seobkim <kks7824@gmail.com>
2022-03-11 00:18:59 +0900

    add readme file

---
+++ readme.md
@@ -0,0 +1,3 @@
+- Springboot2 와 AWS로 구현하는 웹서비스
+- Spring Data JPA 학습
+- Enum class , Oauth2 학습할 것
--- src/main/java/com/jojoldu/book/springboot/domain/user/User.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -4,8 +4,6 @@ import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.cfg.annotations.Nullability;
-import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 
@@ -27,6 +25,10 @@ public class User extends BaseTimeEntity {
     @Column
     private String picture;
 
+    // JPA로 데이터베이스를 저장할 떄 Enum 값을 어떤 형태로 저장할지를 결정
+    // 기본적으로 int로 된 숫자가 저장
+    // 숫자로 저장되면 데이터 베이스로 확인할 때 그 값이 무슨 코드를 의미하는지 알 수가 없음.
+    // 그래서 문자열 (EnumType.STRING)로 저장 될수 있도록 선언
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
@@ -37,6 +39,18 @@ public class User extends BaseTimeEntity {
         this.email = email;
         this.picture = picture;
         this.role = role;
-
     }
+
+    public User update(String name, String picture){
+
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
 }
