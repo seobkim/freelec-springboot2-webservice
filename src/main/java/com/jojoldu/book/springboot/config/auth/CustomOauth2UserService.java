899ee1d221908f27e1cf21e0bddb0c2c7d641a07
seobkim <kks7824@gmail.com>
2022-03-14 22:55:21 +0900

    184p Oauth user service 작성

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOauth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOauth2UserService.java
@@ -1,14 +1,19 @@
 package com.jojoldu.book.springboot.config.auth;
 
+import com.jojoldu.book.springboot.domain.user.User;
 import com.jojoldu.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +24,47 @@ public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        return null;
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+
+        // 현재 로그인 진행 준인 서비스를 구분하는 코드
+        // 지금은 구글만 사용하는 불필요한 값이지만, 이후 네이버 로그인 연동 시에 네이버 로그인인지, 구글 로그인인지 구분하기 위해 사용
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // Oauth2 로그인 진행 시 키가 되는 필드값을 의미 PrimaryKey와 같은 의미
+        // 구글의 경우 기본적인 코드를 지원하지만, 네이버 카카오 등은 기본 지원하지 않음 구글의 기본 코드는 "sub"
+        // 이후 네이버 로그인과 구글 로그인을 동시 지원할 때 사용
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스.
+        // 이후 다른 소셜 로그인도 이 클래스를 사용
+        OAuthAttributes attributes = OauthAttributes.of(registrationId,userNameAttributeName,oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+
+        // sessionUser
+        // 세션에 사용자 정보를 저장하기 위한 Dto 클래스
+        // User 클래스를 세션에 저장하려면 직렬화를 구현하지 않았다는 에러가 남
+        // User 클래스가 엔티티이기 때문에 다른 엔티티와 관계가 형성이 될 수 있음
+        // 직렬화 대상에 여러 엔티티가 포함되니 !! 성능 이슈와 부수효과 !! 가 발생할 확률이 높기 때문에
+        // 직렬화 기능을 가진 세션 Dto를 하나 추가로 만드는 것이 이후 운영 및 유지보수 때 많은 도움이 됨
+        httpSession.setAttribute("user",new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(new
+                SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+
     }
+
+    private User saveOrUpdate(OAuthAttributes attributes){
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
+
 }
